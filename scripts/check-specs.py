54e629ec7d1fb1744856eac72ebe0512fbbcb03f
Iryna Shcherbina <ishcherb@redhat.com>
2017-07-24 17:57:44 +0200

    check-specs script: Check how many of packages have epel branches

--- scripts/check-specs.py
+++ scripts/check-specs.py
@@ -8,8 +8,11 @@ for rhel/epel.
 This check is pretty naive and gives just general information.
 It checks spec files for using `%if %{?rhel}` conditionals
 and may have false positives.
+
+Usage: ./scripts/check-specs.py
 """
 import click
+import json
 import logging
 import os
 import urllib.request
@@ -51,15 +54,36 @@ def check_spec(package):
     try:
         response = urllib.request.urlopen(spec_url)
     except urllib.error.URLError as err:
-        logger.error('Failed to get spec {}. Error: {}'.format(spec_url, err))
+        logger.error('Failed to get spec %s. Error: %s', spec_url, err)
     else:
         spec = str(response.read())
         for marker in MARKERS:
             if marker in spec:
-                logger.warning('{} uses {}: {}'.format(
-                    package.name, ', '.join(MARKERS), spec_url))
+                logger.warning('%s: %s', package.name, spec_url)
                 return package
-    logger.debug('Checked spec for {}: OK'.format(package.name))
+    logger.debug('Checked spec for %s: OK', package.name)
+
+
+def check_branches(package):
+    """Given the package, check if it is built for el6 or epel7.
+
+    Arguments:
+        - package (Package object)
+
+    Return: package if built for el6 or epel7, None otherwise
+    """
+    api_url = PKGDB_API.format(package.name)
+    try:
+        response = urllib.request.urlopen(api_url)
+    except urllib.error.URLError as err:
+        logger.error('Failed to get package info %s. Error: %s', api_url, err)
+    else:
+        response = response.read()
+        result = json.loads(response.decode())
+        branches = [pkg['collection']['branchname'] for pkg in result['packages']]
+        if 'el6' in branches or 'epel7' in branches:
+            logger.debug('%s has epel branch', package.name)
+            return package
 
 
 def check_packages(packages, check_function):
@@ -86,6 +110,7 @@ def main(db):
     db = get_portingdb(db)
     _, data = get_naming_policy_progress(db)
 
+    logger.info('Checking spec files for using %s...', ', '.join(MARKERS))
     result = {}
     for key, packages in data:
         result[key[0].ident] = check_packages(packages, check_spec)
@@ -94,11 +119,19 @@ def main(db):
     total_cross_platform = sum(len(packages) for packages in result.values())
     percentage = total_cross_platform * 100 / total
 
-    logger.info('Checking spec files')
+    logger.info('Checking for epel branches...')
+    pkgdb_result = {}
+    for category, packages in result.items():
+        pkgdb_result[category] = check_packages(packages, check_branches)
+
     print('\nPackages that use {} in spec files: {} of {} ({:.2f}%)'.format(
         ', '.join(MARKERS), total_cross_platform, total, percentage))
     for category, packages in result.items():
         print('  {}: {}'.format(category, len(packages)))
+
+    total_epel = sum(len(packages) for packages in pkgdb_result.values())
+    print('From those {}, have el6 or epel7 branch: {}'.format(
+        total_cross_platform, total_epel))
 
 
 if __name__ == '__main__':
