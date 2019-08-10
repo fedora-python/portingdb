d3a027075e653e37194968525ca9a8b867dec3d9
Miro Hrončok <miro@hroncok.cz>
2019-08-10 22:45:49 +0200

    Apply knowledge from Flock: Check uninstallable packages in seconds

--- portingdb/check_fti.py
+++ portingdb/check_fti.py
@@ -1,27 +1,15 @@
 import json
 import pathlib
 import re
-import shlex
 import subprocess
 import sys
-from functools import partial
+from datetime import datetime
 
 import click
 
 from portingdb.load_data import get_data
 
-
-ROOT_LOG = pathlib.Path('/var/lib/mock/fedora-rawhide-x86_64/result/root.log')
-RE_INSTALLED = re.compile(r'DEBUG util.py:\d+:\s+Installed:')
-RE_COMPLETE = re.compile(r'DEBUG util.py:\d+:\s+Complete!')
-RE_ERROR = re.compile(r'DEBUG util.py:\d+:\s+BUILDSTDERR: Error:\s*')
-RE_SKIP_BROKEN = re.compile(r"DEBUG util.py:\d+:\s+\(try to add.*")
-
-
-def mock(*command, **kwargs):
-    command = ('mock', '-r', 'fedora-rawhide-x86_64') + command
-    print(f'$ {" ".join(shlex.quote(p) for p in command)}')
-    return subprocess.run(command, text=True, capture_output=True, **kwargs)
+CANNOT_RE = re.compile(r"can't install ((.+)-[^-]+-[^-]+):")
 
 
 def pkg_names(data):
@@ -31,83 +19,63 @@ def pkg_names(data):
                 yield nevr.rsplit('-', 2)[0]
 
 
-def was_installed():
-    capturing = False
-    packages = set()
-    for line in ROOT_LOG.read_text().splitlines():
-        if not capturing:
-            if RE_INSTALLED.match(line):
-                capturing = True
-            continue
-        else:
-            if RE_COMPLETE.match(line):
-                return packages
-            line = line.strip().split()
-            assert line[0] == 'DEBUG'
-            assert line[1].startswith('util.py:')
-            for nevra in line[2:]:
-                packages.add(nevra.rsplit('-', 2)[0])
-    else:
-        raise AssertionError('No start or end matched in log')
+def installcheck(repo, arch):
+    solvecache = pathlib.Path(f'/var/cache/dnf/{repo}.solv')
+    filenames = pathlib.Path(f'/var/cache/dnf/{repo}-filenames.solvx')
+    if not (solvecache.exists() and filenames.exists()):
+        print('Fetch the cache first:\n'
+              f'  $ sudo dnf --repo={repo} makecache', file=sys.stderr)
+        sys.exit(1)
+    today = datetime.today()
+    age = max(
+        today - datetime.fromtimestamp(solvecache.stat().st_mtime),
+        today - datetime.fromtimestamp(filenames.stat().st_mtime)
+    )
+    if age.days > 1:
+        print('Cache older than a day, update it via:\n'
+              f'  $ sudo dnf --repo={repo} makecache', file=sys.stderr)
 
+    try:
+        cmd = ('installcheck', arch, str(solvecache), str(filenames))
+        cp = subprocess.run(cmd, text=True, capture_output=True, check=False)
+    except FileNotFoundError:
+        print('Install /usr/bin/installcheck first:\n'
+              '  $ sudo dnf install /usr/bin/installcheck', file=sys.stderr)
+        sys.exit(1)
 
-def eprint_install_error():
-    capturing = False
-    for line in ROOT_LOG.read_text().splitlines():
-        if not capturing:
-            if RE_ERROR.match(line):
-                if 'Unable to find a match' in line:
-                    print(line)
-                    print()
-                    return
-                capturing = True
-            continue
+    packages = {}
+
+    for line in cp.stdout.splitlines():
+        if line.startswith("can't install "):
+            nevra, name = CANNOT_RE.match(line).groups()
+            packages[name] = {
+                'nevra': nevra,
+                'problems': [],
+            }
         else:
-            if RE_SKIP_BROKEN.match(line):
-                print()
-                return
-            if 'BUILDSTDERR:' in line:
-                line = line[line.index('BUILDSTDERR:')+14:]
-                print(line)
+            packages[name]['problems'].append(line)
+
+    return packages
 
 
 @click.command(name='check-fti')
+@click.option('--repo', default='rawhide', show_default=True, metavar='REPO',
+              help='What repository to check')
+@click.option('--arch', default='x86_64', show_default=True, metavar='ARCH',
+              help='What architecture to check')
 @click.argument('results', type=click.Path(writable=True),
-                default='installable.json')
+                default='noninstallable.json')
 @click.pass_context
-def check_fti(ctx, results):
+def check_fti(ctx, repo, arch, results):
     """Check all Python 2 packages to whether they install"""
     data = get_data(*ctx.obj['datadirs'])
+    PKG_NAMES = set(pkg_names(data))
     results = pathlib.Path(results)
 
-    if results.exists():
-        installable = json.loads(results.read_text())
-    else:
-        installable = {}
+    filtered = {k: v for k, v in installcheck(repo, arch).items()
+                if k in PKG_NAMES}
 
-    investigate = set()
+    results.write_text(json.dumps(filtered,  indent=2))
 
-    PKG_NAMES = set(pkg_names(data))
-
-    for pkg in PKG_NAMES:
-        if pkg in installable:
-            continue
-        mock('init', check=True)
-        cp = mock('install', pkg)
-        if cp.returncode == 0:
-            installed_pkgs = was_installed()
-            if pkg not in installed_pkgs:
-                investigate.add(pkg)
-            installed_pkgs &= PKG_NAMES
-            print('Installed', ', '.join(installed_pkgs), '\n')
-            for p in installed_pkgs:
-                installable[p] = True
-        elif cp.returncode == 30:
-            installable[pkg] = False
-            eprint_install_error()
-        else:
-            investigate.add(pkg)
-        results.write_text(json.dumps(installable))
-
-    if investigate:
-        print('Investigate', ', '.join(investigate))
+    print(f'Results in {results}\n'
+          f'There are {len(filtered)} noninstallable Python 2 packages.')
